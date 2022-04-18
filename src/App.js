6beed18afc2ab347834870c0f067d15c49525733
rvaz-dev <rvazn.ds@gmail.com>
2022-04-18 10:31:18 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -2,30 +2,37 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Footer from './components/layout/Footer'
 import Navbar from './components/layout/Navbar'
+import Alert from './components/layout/Alert'
 import About from './pages/About'
 import Home from './pages/Home'
+import User from './pages/User'
 import NotFound from './pages/NotFound'
 import { GithubProvider } from './context/github/GithubContext'
+import { AlertProvider } from './context/alert/AlertContext'
 
 function App() {
 	return (
 		<GithubProvider>
-			<Router>
-				<div className='flex flex-col justify-between h-screen'>
-					<Navbar />
-					<main className='container mx-auto px-3 pb-12 pt-12 flex-grow'>
-						<Routes>
-							<Route exact path='/' element={<Home /> }/>
-							<Route path='/about'  element={<About />} />
-							<Route path='/notfound' element={<NotFound />} />
-							<Route exact path='/*' element={<NotFound /> }/>
-						</Routes>
-					</main>
-					<Footer />
-				</div>
-			</Router>
+			<AlertProvider>
+				<Router>
+					<div className='flex flex-col justify-between h-screen'>
+						<Navbar />
+						<main className='container mx-auto px-3 pb-12 pt-12 flex-grow'>
+							<Alert />
+							<Routes>
+								<Route exact path='/' element={<Home /> }/>
+								<Route path='/about' element={<About />} />
+								<Route path='/user/:login'  element={<User />} />
+								<Route path='/notfound' element={<NotFound />} />
+								<Route exact path='/*' element={<NotFound /> }/>
+							</Routes>
+						</main>
+						<Footer />
+					</div>
+				</Router>
+			</AlertProvider>
 		</GithubProvider>
-    )
+)
 }
 
 export default App
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -7,49 +7,82 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
 export const GithubProvider = ({ children }) => {
-    const initialState = {
-        users: [],
-        loading: false,
-    }
+	const initialState = {
+		users: [],
+		user: {},
+		loading: false,
+	}
 
-    const [state, dispatch] = useReducer(githubReducer, initialState)
+	const [state, dispatch] = useReducer(githubReducer, initialState)
 
+	// Get search results
+	const searchUsers = async (text) => {
+		setLoading()
 
-    // Get search results
-    const searchUsers = async (text) => {
-        setLoading()
+		const params = new URLSearchParams({
+			q: text,
+		})
 
-        const params = new URLSearchParams({
-            q: text
-        })
+		const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+			headers: {
+				Authorization: `token: ${GITHUB_TOKEN}`,
+			},
+		})
 
-        const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-            headers: {
-                Authorization: `token: ${GITHUB_TOKEN}`
-            }
-        })
+		const { items } = await response.json()
 
-        const { items } = await response.json()
+		dispatch({
+			type: 'GET_USERS',
+			payload: items,
+		})
+	}
 
-        dispatch({
-            type: 'GET_USERS',
-            payload: items,
-        })
-    }
+	// Get a single user
+	const getUser = async (login) => {
+		setLoading()
 
-    // Set Loading...
-    const setLoading = () => {dispatch({type:'SET_LOADING'})}
+		const response = await fetch(`${GITHUB_URL}/search/users/${login}`, {
+			headers: {
+				Authorization: `token: ${GITHUB_TOKEN}`,
+			},
+		})
 
+		if (response.status === 404) {
+			window.location = '/notfound'
+		} else {
+			const data = await response.json()
 
+			dispatch({
+				type: 'GET_USER',
+				payload: data,
+			})
+		}
+	}
 
-    return <GithubContext.Provider value={{
-        users: state.users,
-        loading: state.loading,
-        searchUsers,
-    }}>
-        {children}
-    </GithubContext.Provider>
+	// Clear users from state
+	const clearUsers = () => {
+		dispatch({ type: 'CLEAR_USERS' })
+	}
 
+	// Set Loading...
+	const setLoading = () => {
+		dispatch({ type: 'SET_LOADING' })
+	}
+
+	return (
+		<GithubContext.Provider
+			value={{
+				users: state.users,
+				loading: state.loading,
+				user: state.user,
+				searchUsers,
+                clearUsers,
+                getUser,
+			}}
+		>
+			{children}
+		</GithubContext.Provider>
+	)
 }
 
 export default GithubContext
